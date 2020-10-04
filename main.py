ac36070bf1855a73e106e4cd51ab478d3295422c
TheoMorkel <theo.jnr.morkel@gmail.com>
2020-10-04 18:51:29 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import makefileGenerator as mG
+
+
+# set the path to the cpp files
+mG.setFolderDir("D:\\Skoolwerk\\2020\\Semester 2\\COS 214\\Practicals\\Practical 4\\Task1")
+
+# Create makefile by specifying the main
+mG.makeMakefile("test")
